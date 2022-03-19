ba282be633300554b785d7f76261e9a738bd4c72
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2022-03-19 19:34:32 +0530

    Updated About Section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <img className="about-img" src={img}/>
       </div>
       <div className="about-content">
-        <h2 className="about-heading">About</h2>
+        <div className="about-heading">About</div>
         <p className="about-info">
         Traditionally, the purpose of college education has been to create individuals who know everything about something. Unfortunately, this methodology also results in individuals who know little about anything else, except their subject. At NUV, we aim to create T-shaped personalities. Our students have working knowledge about a wide range of subjects and deep domain knowledge in one area of specialisation. We enable this through three synergies - Synergy of Theory & Practice, Synergy of Faculty & Industry, and Synergy of Youth & Experience.
         </p>
