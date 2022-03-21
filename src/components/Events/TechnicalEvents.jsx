3c44c85dc464340fba9c1049e2005ad9cef02c8a
sheelahirrao16 <18103512@nuv.ac.in>
2022-03-21 19:11:40 +0530

    Feature: Event Subpages (Update Progress)

--- src/components/Events/TechnicalEvents.jsx
+++ src/components/Events/TechnicalEvents.jsx
@@ -3,25 +3,43 @@ import '../../styles/Events.css'
 import Slider from './Slider'
 import { EventsData } from '../EventCategory/EventsData'
 import './styles/TechnicalEvents.css'
+import { styles } from './styles/TechnicalElements'
+import { Row, Col, Container, Card, Button } from "react-bootstrap"
 
 const TechnicalEvents = () => {
     return (
         <>
             <div className='technicalevents'>
-                {EventsData.map((val, index) => {
-
-                    const { id, name, image, events } = val
-
-                    console.log(events)
-
+                {EventsData.map(data => {
                     return (
-                        <div className="event-wrapper" key={id}>
+                        <div className="event-wrapper" key={data.id}>
                             <div className="title">
-                                <h4>{name}</h4>
+                                <h4>{data.name}</h4>
                             </div>
-                            <div className="sliders">
-                                <Slider Events={events} />
-                            </div>
+                            <Row md={2}>
+                            {data.events.map(event => {
+                                return (
+                                        <Col style={styles.col_card}>
+                                            <Card style={styles.card} className="card">
+                                                <Card.Img
+                                                    style={styles.card_image}
+                                                    className="card-img"
+                                                    variant="top"
+                                                    // height="350rem"
+                                                    src={event.image}
+                                                />
+                                                <Card.Body style={styles.card_body}>
+                                                    <Card.Title style={styles.card_title}>{event.name}</Card.Title>
+                                                    <Card.Title style={styles.card_subtitle} className='card_subtitle'>
+                                                        {event.price}
+                                                    </Card.Title>
+                                                    <Card.Text style={styles.card_description}>{event.description}</Card.Text>
+                                                </Card.Body>
+                                            </Card>
+                                        </Col>
+                                )
+                            })}
+                            </Row>
                         </div>
                     )
                 })[0]}
---
+++ src/components/Events/styles/TechnicalElements.js
@@ -0,0 +1,68 @@
+export const styles = {
+    box: {
+      padding: "1rem 4rem ",
+    },
+  
+    col_card: {
+      display: "flex",
+      justifyContent: "center",
+      alignItem: "center",
+  
+      padding: "0.8rem",
+    },
+    header: {
+      display: "flex",
+      justifyContent: "center",
+      alignItem: "center",
+    },
+    title: {
+      color: " #f3f3f7",
+      fontSize: "3rem",
+      fontHeight: 700,
+      letterSpacing: " 1.4px",
+      textTransform: "uppercase",
+    },
+    subtitle: {
+      color: " #f3f3f7",
+      fontSize: "2rem",
+      fontHeight: 700,
+      letterSpacing: " 1.4px",
+      textTransform: "uppercase",
+      marginBottom: " 3rem",
+    },
+    card: {
+      width: "21rem",
+      borderTopRightRadius: "20px",
+      borderTopLeftRadius: "20px",
+      borderBottomRightRadius: "20px",
+      borderBottomLeftRadius: "20px",
+      backgroundColor: "black",
+      padding: "0rem",
+    },
+
+    card_image: {
+      borderTopRightRadius: "20px",
+      borderTopLeftRadius: "20px",
+    },
+
+    card_title: {
+      display: "flex",
+      justifyContent: "center",
+      alignItem: "center",
+      color: " #f3f3f7",
+    },
+    card_subtitle: {
+      fontFamily: "'Courier New' , monospace ",
+      display: "flex",
+      justifyContent: "center",
+      alignItem: "center",
+      color: " #f3f3f7",
+    },
+
+    card_description: {
+      display: "flex",
+      justifyContent: "center",
+      alignItem: "center",
+      color: " #f3f3f7",
+    },
+  };
